c4fe7dd53e4d470213d832c663d02871bd76acd1
GrigorovBore <borislav15.grigorov@gmail.com>
2026-07-17 14:13:58 +0300

    Add hostname display

--- src/main.py
+++ src/main.py
@@ -1,2 +1,15 @@
+# 1. Imports
+
+import socket
+
+# 2. Functions
+
+def get_hostname():
+    return socket.gethostname()
+
+# 3. Main Program
+
 print("ServerWatch v1.0")
 print("================")
+
+print(f'Hostname: {get_hostname()}')
