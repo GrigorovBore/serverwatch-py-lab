b3a468994eeb18ca6bda5bc95ff8b4416b5bf18c
GrigorovBore <borislav15.grigorov@gmail.com>
2026-07-31 21:25:23 +0300

    Return monitoring data as JSON

--- src/web_service.py
+++ src/web_service.py
@@ -13,17 +13,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return (
-        f"1. Hostname:  {get_hostname()}<br>"
-        f"2. OS:        {get_operating_system()}<br>"
-        f"3. CPU usage: {get_cpu_usage()}%<br>"
-        f"4. Memory usage: {get_memory_usage()}%<br>"
-        f"5. Disk usage: {get_disk_usage()}%<br>"
-        f"6. Uptime: {get_uptime()}<br>"
-        f"7. Failed services: {get_failed_services()}"
-
-    )
+    return {
+        "hostname": get_hostname(),
+        "operating_system": get_operating_system(),
+        "cpu_usage": get_cpu_usage(),
+        "memory_usage": get_memory_usage(),
+        "disk_usage": get_disk_usage(),
+        "uptime": get_uptime(),
+        "failed_services": get_failed_services(),
+    }
 
 if __name__ == "__main__":
     app.run(debug=True)
-
